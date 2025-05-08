9de375fbbfcdb3d1617f053badb58ee3ca5be740
Asora7 <verstr01474@stud.noroff.no>
2025-05-08 14:16:13 +0200

    created layout.tsx

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+// src/components/Layout.tsx
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout: React.FC = () => (
+  <>
+    <Navbar />
+    <main className="container my-5">
+      <Outlet />
+    </main>
+    <Footer />
+  </>
+);
+
+export default Layout;
